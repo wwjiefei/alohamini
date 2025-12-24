3c3e4bc8273991e10a6814ea1dc8b873061d9013
ZhaiShuaiShuai <18264798931@163.com>
2025-12-24 13:44:02 +0800

    Remove lerobot_alohamini to clear LFS history

--- lerobot_alohamini/examples/debug/test_network.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-try:
-    response = requests.get("https://www.google.com")
-    print("Google Connection successful:", response.status_code)
-    response = requests.get("https://huggingface.co")
-    print("Huggingface Connection successful:", response.status_code)
-except requests.ConnectionError as e:
-    print("Connection failed:", e)
